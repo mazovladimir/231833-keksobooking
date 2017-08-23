cdba388748c5987aa50ebde25e709fd872061b09
mazovladimir@gmail.com <mazovladimir@gmail.com>
2017-08-23 14:37:30 +0300

    update 6

--- js/map.js
+++ js/map.js
@@ -5,15 +5,19 @@ var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var ADDRESS = '{{getX()}}, {{getY()}}';
 var ADS_COUNT = 8;
+var GUESTS_COUNT = 100;
 
 var fragment = document.createDocumentFragment();
+var similarAdsTemplate = document.querySelector('#lodge-template').content;
 
 var myads = [];
 
 for (var i = 0; i < ADS_COUNT; i++) {
   myads[i] = new Ads();
+  myads[i].author.avatar = getAvatar(i + 1);
+  myads[i].offer.title = TITLE[i];
+  myads[i].offer.address = myads[i].mylocation.x + ',' + myads[i].mylocation.y;
 }
 
 function getRandom(max, min) {
@@ -23,25 +27,25 @@ function getRandom(max, min) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getAvatar() {
-  return 'img/avatars/user' + '0' + getRandom(ADS_COUNT - 1, 1) + '.png';
+function getAvatar(element) {
+  return 'img/avatars/user' + '0' + element + '.png';
 }
 
 function Ads() {
   this.author = {
-    avatar: getAvatar(),
+    avatar: '',
   };
   this.offer = {
-    title: TITLE,
-    address: ADDRESS,
-    price: getRandom(100000000, 1000),
-    type: TYPE,
+    title: '',
+    address: '',
+    price: getRandom(1000000, 1000),
+    type: TYPE[getRandom(TYPE.length - 1)],
     rooms: getRandom(5, 1),
-    guests: getRandom(100),
-    checkin: CHECKIN,
-    checkout: CHECKOUT,
-    features: FEATURES,
-    description: ' ',
+    guests: getRandom(GUESTS_COUNT),
+    checkin: CHECKIN[getRandom(CHECKIN.length - 1)],
+    checkout: CHECKOUT[getRandom(CHECKOUT.length - 1)],
+    features: FEATURES[getRandom(FEATURES.length - 1)],
+    description: '',
     photos: [],
   };
   this.mylocation = {
@@ -49,6 +53,12 @@ function Ads() {
     y: getRandom(500, 100),
   };
 }
+
+var renderAds = function (ads) {
+  var adsElement = similarAdsTemplate.cloneNode(true);
+  adsElement.querySelector('.lodge__title').textContent = ads.offer.title[0];
+  return adsElement;
+};
 
 myads.forEach(function (ads) {
   var newElement = document.createElement('div');
@@ -58,4 +68,4 @@ myads.forEach(function (ads) {
   fragment.appendChild(newElement);
 });
 
-document.querySelector(".tokyo__pin-map").appendChild(fragment);
+document.querySelector('.tokyo__pin-map').appendChild(fragment);
