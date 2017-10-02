828f950dc54822530dc26ae22e79031620b91e3f
mazovladimir@gmail.com <mazovladimir@gmail.com>
2017-10-02 12:05:43 +0300

    update 1

--- js/map.js
+++ js/map.js
@@ -1,4 +1,44 @@
 'use strict';
 
 window.map = (function () {
+  var address = document.querySelector('#address');
+  var pinMain = document.querySelector('.pin__main');
+  address.readOnly = true;
+
+  pinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    function mouseMove(moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
+      pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
+      address.value = 'x:' + startCoords.x + ', y:' + startCoords.y;
+    }
+
+    function mouseUp(upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', mouseMove);
+      document.removeEventListener('mouseup', mouseUp);
+    }
+
+    document.addEventListener('mousemove', mouseMove);
+    document.addEventListener('mouseup', mouseUp);
+  });
 })();
