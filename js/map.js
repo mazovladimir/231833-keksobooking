6ecc04b2af5e65b2cedcc3356d9a88b4f9ef4897
mazovladimir@gmail.com <mazovladimir@gmail.com>
2017-10-05 21:09:03 +0300

    update 4

--- js/map.js
+++ js/map.js
@@ -3,42 +3,45 @@
 window.map = (function () {
   var address = document.querySelector('#address');
   var pinMain = document.querySelector('.pin__main');
+  var startCoords;
 
   pinMain.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
+    mouseDown(evt);
+  });
 
-    var startCoords = {
-      x: evt.pageX,
-      y: evt.pageY
+  function mouseDown(evt) {
+    startCoords = {
+      x: evt.cleintX,
+      y: evt.clientY
     };
-
-    function mouseMove(moveEvt) {
-      moveEvt.preventDefault();
-
-      var shift = {
-        x: startCoords.x - moveEvt.pageX,
-        y: startCoords.y - moveEvt.pageY
-      };
-
-      startCoords = {
-        x: moveEvt.pageX,
-        y: moveEvt.pageY
-      };
-
-      pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
-      pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
-
-      address.value = 'x:' + (parseInt(pinMain.style.left) + 36) + ', y:' + pinMain.style.top;
-    }
-
-    function mouseUp(upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener('mousemove', mouseMove);
-      document.removeEventListener('mouseup', mouseUp);
-    }
 
     document.addEventListener('mousemove', mouseMove);
     document.addEventListener('mouseup', mouseUp);
-  });
+  }
+
+  function mouseMove(moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
+    pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
+
+    address.value = 'x:' + (parseInt(pinMain.style.left, 10) + 36) + ', y:' + (parseInt(pinMain.style.top, 10) + 94);
+  }
+
+  function mouseUp(upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', mouseMove);
+    document.removeEventListener('mouseup', mouseUp);
+  }
 })();
